0d3563f6bc8abbb09a570be9891af5580f12f758
Ramazan <62503301+multitown25@users.noreply.github.com>
2023-06-01 08:41:48 +0300

    update

--- src/components/PositionList/PostitionList.jsx
+++ src/components/PositionList/PostitionList.jsx
@@ -20,6 +20,37 @@ const position = [
     {id: '6', title: 'Чай чер/зел', price: 80, img: tea}
 ]
 
+const salads = [
+    {id: '1', title: 'Цезарь с кур', price: 100, img: espresso},
+    {id: '2', title: 'Цезарь с крев', price: 150, img: americano},
+    {id: '3', title: 'Греческий', price: 200, img: capuchino},
+    {id: '4', title: 'Новогодний', price: 250, img: redbull},
+    {id: '5', title: 'Летний', price: 50, img: water}
+]
+
+const soups = [
+    {id: '1', title: 'Лапша', price: 100, img: espresso},
+    {id: '2', title: 'Борщ', price: 150, img: americano},
+    {id: '3', title: 'Сырный крем-суп', price: 200, img: capuchino},
+    {id: '4', title: 'Грибной крем-суп', price: 250, img: redbull}
+]
+
+const pasta = [
+    {id: '1', title: 'Спагетти с гов', price: 100, img: espresso},
+    {id: '2', title: 'Спагетти с кур', price: 150, img: americano},
+    {id: '3', title: 'Карбонара', price: 200, img: capuchino},
+    {id: '4', title: 'Пенне с бек и кур', price: 250, img: redbull}
+]
+
+const breakfast = [
+    {id: '1', title: 'Омлет по-мексикански', price: 100, img: espresso},
+    {id: '2', title: 'Гурман', price: 150, img: americano},
+    {id: '3', title: 'Омлет торнадо', price: 200, img: capuchino},
+    {id: '4', title: 'Английский завтрак', price: 250, img: redbull},
+    {id: '5', title: 'Ванильные сырники', price: 50, img: water},
+    {id: '6', title: 'Фитнес', price: 80, img: tea}
+]
+
 const getTotalPrice = (items = []) => {
     return items.reduce((acc, item) => {
         return acc += item.price
@@ -53,27 +84,26 @@ const PositionList = () => {
     // }, [onSendData])
 
     const onAdd = (food) => {
-        const exist = addedItems.find((x) => x.id === food.id);
-        if (exist) {
+        const alreadyAdded = addedItems.find(item => item.id === food.id);
+        let newItems = [];
+
+        if(alreadyAdded) {
             console.log('exist')
-            setAddedItems(
-                addedItems.map((x) =>
-                    x.id === food.id ? { ...exist, quantity: exist.quantity + 1 } : x
-                )
-            );
+            newItems = addedItems.filter(item => item.id !== food.id);
         } else {
             console.log('no exist')
-            setAddedItems([...addedItems, { ...food, quantity: 1 }]);
+            newItems = [...addedItems, food];
         }
 
+        setAddedItems(newItems)
+
         console.log(addedItems.length)
-        if(addedItems.length === 0) {
-            tg.MainButton.  hide();
+        if(newItems.length === 0) {
+            tg.MainButton.hide();
         } else {
-            console.log(getTotalPrice(addedItems));
             tg.MainButton.show();
             tg.MainButton.setParams({
-                text: `Купить ${getTotalPrice(addedItems)}`
+                text: `Купить ${getTotalPrice(newItems)}`
             })
         }
     };

868986a1da48a0c5ea79b16228fd0ca22893343e
Ramazan <62503301+multitown25@users.noreply.github.com>
2023-06-05 15:16:14 +0300

    update

--- src/db/db.js
+++ src/db/db.js
@@ -1,7 +1,7 @@
 export async function getCategories() {
     const token = localStorage.getItem('auth-token');
     console.log(token);
-    const categories = await fetch('5.101.51.105/categories', {
+    const categories = await fetch('5.101.51.105/api/category', {
         method: 'GET',
         headers:  {
             'Content-type': 'application/json',

29f8796fff7d57b369414e347683a03a134d46f7
Ramazan <62503301+multitown25@users.noreply.github.com>
2023-06-05 18:13:19 +0300

    update

--- src/components/CategoryItem/CategoryItem.jsx
+++ src/components/CategoryItem/CategoryItem.jsx
@@ -28,7 +28,7 @@ const CategoryItem = ({category, className, onAdd}) => {
     return (
         <div className={'category ' + className}>
             <div className={'image__container'}>
-                <img src={category.img} onClick={imageClick}/>
+                <img src={category.imgSrc} onClick={imageClick}/>
             </div>
             <h4 className="card__title__category">{category.name}</h4>
             {/*<div className={'title'}>{category.title}</div>*/}

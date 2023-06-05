bba9fba280d08331923fe629380b005138192459
Ramazan <62503301+multitown25@users.noreply.github.com>
2023-06-05 16:29:01 +0300

    update

--- src/db/db.js
+++ src/db/db.js
@@ -3,10 +3,10 @@ import axios from "axios";
 export async function getCategories() {
     // const token = localStorage.getItem('auth-token');
     // console.log(token);
-    axios({
+    const categories = await axios({
         method: "get",
         url: "http://localhost:8000/getCategories",
-    }).then(function (response) {
-        console.log(response.data);
     });
+
+    return await categories.data;
 }

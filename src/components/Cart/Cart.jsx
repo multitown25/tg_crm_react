c15d5642a6c527896757332b407db2893aed0344
Ramazan <62503301+multitown25@users.noreply.github.com>
2023-06-14 12:13:04 +0300

    update

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import "./Cart.css";
 import Button from "../Button/Button";
+import {useTelegram} from "../../hooks/useTelegram";
 function Cart({ addedItems, onCheckout }) {
     const totalPrice = addedItems.reduce((a, c) => a + c.price * c.quantity, 0);
+    const {user, queryId} = useTelegram();
 
     return (
         <div className="cart__container">
             {addedItems.length === 0 ? "В заказе нет позиций" : ""}
-            <br /> <span className="">Итого: {totalPrice.toFixed(2)}Р</span>
+            <br /> <span className="">Итого:{user?.username} {queryId} {totalPrice.toFixed(2)}Р</span>
             <Button
                 title={`${addedItems.length === 0 ? "Заказать" : "Оплатить"} `}
                 type={"checkout"}
